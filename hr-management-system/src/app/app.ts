1dbed88ed37b0916571702caafc988b50686b0f9
Eurekab@vertice.software <eurekaripfumelo@gmail.com>
2026-03-10 00:02:28 +0200

    Login page setup

--- hr-management-system/src/app/app.ts
+++ hr-management-system/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent],
   template: `
-    <h1>Hello, {{ title() }}</h1>
-
+    <app-login/>
     <router-outlet />
   `,
   styles: [],

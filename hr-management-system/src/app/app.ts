6ee76f9e4c77a3c1242a3c73cf14f09ba6882f2f
eureka03 <eurekaripfumelo@gmail.com>
2026-03-11 22:53:06 +0200

    Homepage changes

--- hr-management-system/src/app/app.ts
+++ hr-management-system/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet,LoginComponent, HomepageComponent],
   template: `
+    <app-homepage/>
     <app-login/>
     <router-outlet />
   `,

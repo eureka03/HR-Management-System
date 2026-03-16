c1051d44b642c2603b842df2b8a569d5b07f44a2
eureka03 <eurekaripfumelo@gmail.com>
2026-03-16 11:53:49 +0200

    route changes

--- hr-management-system/src/app/app.routes.ts
+++ hr-management-system/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 export const routes: Routes = [
+    { path: '', component: HomepageComponent },
+    { path: 'login', component: LoginComponent }
     
 ];

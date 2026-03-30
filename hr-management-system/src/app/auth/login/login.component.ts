f719689e60431afcd92595885579485e30b02402
eureka03 <eurekaripfumelo@gmail.com>
2026-03-30 15:18:49 +0200

    Adding validation errors

--- hr-management-system/src/app/auth/login/login.component.ts
+++ hr-management-system/src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { FormGroup, ReactiveFormsModule,FormControl,Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+
 
 
 @Component({
@@ -13,7 +15,7 @@ import { FormGroup, ReactiveFormsModule,FormControl,Validators } from "@angular/
 export class LoginComponent{
     isLoggedIn = false;
 
-    constructor(private router: Router){}
+    constructor(private router: Router, private authService:AuthService){}
     loginForm = new FormGroup(
         {
             email: new FormControl("",[Validators.required, Validators.email]),
@@ -23,13 +25,14 @@ export class LoginComponent{
 
     //Connect the form to a user model when loging in
     onSubmit(){
-        
+
+        //We are going to use the values from the form to create a login request object that we will send to the backend
+        const email = this.loginForm.get('email')?.value;
+        const password = this.loginForm.get('password')?.value;
+
         if(this.loginForm.valid){
             this.router.navigate(['/dashboard']);
             this.isLoggedIn = true;
         }
     }
-
-    
-
 }
